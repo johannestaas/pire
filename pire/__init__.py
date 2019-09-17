2d6421588d7bd81ec2aeb76cd8b9fcb3a53ccd9a
Johan Nestaas <johannestaas@gmail.com>
2019-09-17 01:32:27 -0700

    add main boilerplate argparser

--- pire/__init__.py
+++ pire/__init__.py
@@ -10,3 +10,19 @@ __all__ = ()
 __author__ = 'Johan Nestaas <johannestaas@gmail.com'
 __license__ = 'GPLv3'
 __copyright__ = 'Copyright 2019 Johan Nestaas'
+
+
+def main():
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--preprocess', '-p',
+        help='',
+    )
+    parser.add_argument(
+        '--regex', '-r',
+        default='regex.pire',
+        help='',
+    )
+    parser.add_argument('input_file')
+    args = parser.parse_args()

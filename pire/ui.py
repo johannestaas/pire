37197d895a28b98728f47231970db46fda80d6b7
Johan Nestaas <johannestaas@gmail.com>
2019-09-18 01:41:00 -0700

    layout and border

--- pire/ui.py
+++ pire/ui.py
@@ -8,16 +8,28 @@ def gen_lines(input_path, preprocess):
         yield from preprocess(f)
 
 
-def display(regexes, regex_win, out_win):
+def display(regexes, scr, regex_win, out_win, match_win):
     regex_win.clear()
     out_win.clear()
+    match_win.clear()
     for i, regex in enumerate(regexes):
         color = 'red' if not regex.enabled else None
         regex_win.write(regex.regex_str, pos=(0, i), color=color)
     regex_win.refresh()
+    w, _ = scr.max_size()
+    out_win.write('=' * (w // 2), pos=(0, 0), color='blue')
+    match_win.write('=' * (w // 2), pos=(0, 0), color='blue')
     for i, out in enumerate(regexes[2].output):
-        msg, color = out.format()
-        out_win.write(msg, pos=(0, i), color=color)
+        pos = (0, i + 1)
+        match_win.write('|', color='blue', pos=pos)
+        if out.match is None:
+            out_win.write(out.line, color='yellow', pos=pos)
+            continue
+        out_win.write(out.line, color=None, pos=pos)
+        groups = out.match.groupdict() or out.match.groups()
+        if groups:
+            match_win.write(repr(groups), pos=(pos[0] + 1, pos[1]))
+    match_win.refresh()
     out_win.refresh()
 
 
@@ -28,18 +40,20 @@ def run_pire(
     split_horiz=True,
 ):
     with Cursed() as scr:
-        width, height = scr.max_size()
-        if split_horiz:
-            size = (width, height // 2)
-            second_orig = (0, height // 2)
-        else:
-            size = (width // 2, height)
-            second_orig = (width // 2, 0)
-        regex_win = scr.new_win(orig=(0, 0), size=size)
-        out_win = scr.new_win(orig=second_orig, size=size)
+        layout = scr.new_layout()
+        layout.add_row(3, [12])
+        layout.add_row(9, [6, 6])
+        layout.draw()
+        rows = list(layout)
+        regex_win = rows[0][0]
+        out_win, match_win = rows[1]
+
         while True:
             regexes = load_regexes(regex_path)
             line_gen = gen_lines(input_path, preprocess)
             run_regexes(regexes, line_gen)
-            display(regexes, regex_win, out_win)
-            scr.getch()
+            scr.clear()
+            scr.refresh()
+            display(regexes, scr, regex_win, out_win, match_win)
+            if scr.getkey() == 'q':
+                break

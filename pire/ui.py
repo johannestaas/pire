d07217c70042b5a81b8b4378cbd3ac44edaf4df9
Johan Nestaas <johannestaas@gmail.com>
2019-09-22 00:29:52 -0700

    fix ui

--- pire/ui.py
+++ pire/ui.py
@@ -9,27 +9,20 @@ def gen_lines(input_paths):
             yield from f
 
 
-def display(regexes, sel_regex, scr, regex_win, out_win, match_wins):
+def display(regexes, sel_regex, scr, regex_win, out_win):
     """
     Displays the input matched against the regexes in two windows.
     """
     regex_win.clear()
     out_win.clear()
-    for win in match_wins:
-        win.clear()
     for i, regex in enumerate(regexes):
         color = None if regex is not sel_regex else 'green'
         regex_win.write(regex.regex_str, pos=(0, i), color=color)
     regex_win.refresh()
     out_w, h = out_win.max_size()
     out_win.write('=' * out_w, pos=(0, 0), color='blue')
-    for win in match_wins:
-        win_w, _ = win.max_size()
-        win.write('=' * win_w, pos=(0, 0), color='blue')
     for i, out in enumerate(sel_regex.output):
         pos = (0, i + 1)
-        for win in match_wins:
-            win.write('|', color='blue', pos=pos)
         if out.match is None:
             out_win.write(out.line, color='yellow', pos=pos)
             continue
@@ -40,11 +33,6 @@ def display(regexes, sel_regex, scr, regex_win, out_win, match_wins):
                 keys = list(groups.keys())
             else:
                 keys = range(len(groups))
-            for i, win in enumerate(match_wins):
-                key = keys[i]
-                win.write(groups[key], pos=(pos[0] + 1, pos[1]))
-    for win in match_wins:
-        win.refresh()
     out_win.refresh()
 
 
@@ -57,23 +45,17 @@ def run_pire(
         while True:
             regexes = load_regexes(regex_path)
             sel_regex = regexes[2]
-            groups = sel_regex.num_groups
-            if groups:
-                group_cols = [8 // groups] * groups
-                match_win_sizes = [4] + group_cols
-            else:
-                match_win_sizes = [12]
             layout = scr.new_layout()
             layout.add_row(2, [12])
-            layout.add_row(10, match_win_sizes)
+            layout.add_row(10, [12])
             layout.draw()
             rows = list(layout)
             regex_win = rows[0][0]
-            out_win, *match_wins = rows[1]
+            out_win = rows[1][0]
             line_gen = gen_lines(input_paths)
             run_regex(sel_regex, line_gen)
             scr.clear()
             scr.refresh()
-            display(regexes, sel_regex, scr, regex_win, out_win, match_wins)
+            display(regexes, sel_regex, scr, regex_win, out_win)
             if scr.getkey() == 'q':
                 break
